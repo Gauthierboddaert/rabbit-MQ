0409a0c13ad522228fe55fdf3d0a9a020b179cb7
Gauthierboddaert <Boddaert.gauthier@gmail.com>
2023-08-19 17:44:25 +0200

    fix: fix dockerfile for using rabbitMQ

---
+++ src/Controller/pdfController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\ResponseInterface;
+
+class pdfController extends AbstractController
+{
+    #[Route('/pdf', name: 'pdf')]
+    public function index(): Response
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/pdfController.php',
+        ]);
+    }
+
+}
